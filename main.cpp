b8b6b4b9693a096fed3d38469a61343b20f9ae04
veghakos01 <veghakos01@gmail.com>
2022-05-09 15:32:32 +0200

    Player widgetek a helyén, frissebb kódnevek

--- main.cpp
+++ main.cpp
@@ -11,20 +11,36 @@ class App : public Application
 {
 private:
     //Scrolltext* s;
-    szamw* z1;
-    szamw* z2;
-    kivalw* k1;
+    szamw* j1_ero;
+    szamw* j1_szog;
+    kivalw* j1_lovedek;
+
+    szamw* j2_ero;
+    szamw* j2_szog;
+    kivalw* j2_lovedek;
+
+
 public:
     App(int w, int h) : Application(w,h)
     {
         //s = new Scrolltext(140,60,1000,400,5,this);
-        z1 = new szamw(150,500,150,50,5,this,42,10,80);
-        z2 = new szamw(150,700,100,50,5,this,80,-100,720);
-        k1 = new kivalw(500,500,120,30,5,this,0);
-        k1->add("A widget 1");
-        k1->add("A  2");
-        k1->add("A widget 3");
-        k1->add("A negy");
+        j1_ero = new szamw(50,50,100,50,5,this,50,0,100);
+        j1_szog = new szamw(50,150,100,50,5,this,45,0,90);
+        j1_lovedek = new kivalw(250,50,120,30,5,this,0);
+        j1_lovedek->add("A widget 1");
+        j1_lovedek->add("A  2");
+        j1_lovedek->add("A widget 3");
+        j1_lovedek->add("A negy");
+        j1_lovedek->add("valami");
+
+        j2_ero = new szamw(W-185,50,100,50,5,this,50,0,100);
+        j2_szog = new szamw(W-185,150,100,50,5,this,45,0,90);
+        j2_lovedek = new kivalw(W-355,50,120,30,5,this,0);
+        j2_lovedek->add("A widget 1");
+        j2_lovedek->add("A  2");
+        j2_lovedek->add("A widget 3");
+        j2_lovedek->add("A negy");
+        j2_lovedek->add("megegy");
     }
 };
 
